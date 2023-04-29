f937a065ed6f33b8ad28c5fd46c155d7fdade125
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-04-29 23:59:55 +0530

    error resolved

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let heading = document.getElementsByClassName(".acc-head");
+let heading = document.getElementsByClassName("acc-head");
 
 
 console.log(heading);
 
-heading.forEach((element) => {
-    element.addEventListener("click", (event) => {
-        console.log(event.target)
-    })    
-});
+// heading.forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//         console.log(event.target)
+//     })    
+// });
+
 
