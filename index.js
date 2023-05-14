05a76313a3342839e353178e89cf94180fae5745
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-05-14 23:26:25 +0530

    toggle style

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ console.log(heading);
 for(let i=0; i<heading.length; i++){
     heading[i].addEventListener("click", (event)=>{
         console.log(event.target)
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
     })
 }
 
