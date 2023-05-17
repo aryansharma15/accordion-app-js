a405c8113f0e5435dd1294bb2cd835ff43fb9844
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-05-17 23:32:21 +0530

    tried another approach, no result

--- index.js
+++ index.js
@@ -4,25 +4,17 @@ const heading = document.getElementsByClassName("acc-head");
 console.log(heading);
 
 
-
-
-// Array.from(heading).forEach((element) => {
-//   element.addEventListener("click", (event) => {
-//     console.log(event.target);
-//   });
-// });
-
 for(let i=0; i<heading.length; i++){
     heading[i].addEventListener("click", (event)=>{
-        console.log(event.target)
+        // console.log(event.target)
         this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
+        
+        let p = this.nextElementSibling;
+        if (p.style.display === "block") {
+          p.style.display = "none";
         } else {
-          panel.style.display = "block";
+          p.style.display = "block";
         }
     })
 }
